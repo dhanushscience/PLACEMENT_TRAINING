38359f5c5297e9af354bc7548d3fd94d7d40de73
dhanushscience <contactdhanush@proton.me>
2025-03-13 12:17:47 +0530

    test

--- PY CODE/DAY_01/sqare.py
+++ PY CODE/DAY_01/sqare.py
@@ -4,3 +4,12 @@ input_num = int(input("Enter a number to find the square and the square root\n")
 print(f"Square of {input_num} = {input_num*input_num}\nSquare root of {input_num} = {math.sqrt(input_num):.5f}\n")
 
 # Why python has not implicit type conversion
+
+
+i = int(input("Enter a number\n"))
+
+while i <= 3:
+    print(f"{i}")
+    i+=1
+else:
+    print(f"Not true condition")
